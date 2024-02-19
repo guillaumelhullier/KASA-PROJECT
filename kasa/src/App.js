92a37e0a3ea3a5c119fff171c0f4085fab420695
guillaumelhullier <89132296+guillaumelhullier@users.noreply.github.com>
2024-02-19 19:02:11 +0100

    start

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Error from "./pages/PageNotFound";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Logement from "./pages/Logement";
 
-function App() {
+//Routes
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="*" element={<Error />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/logements/:id" element={<Logement />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
